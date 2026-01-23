4725fa0b4e4369a154330cb7534e184035e5eaee
Jared Bradley <bradljar@mail.gvsu.edu>
2026-01-23 15:27:06 -0500

    added to player class

--- player.h
+++ player.h
@@ -7,12 +7,28 @@
 class Player {
 	private:
 		std::vector<Card> hand;
+        int money;
+        int currentBet;
+
 	public:
 		Player() = default;
 		Player(std::vector<Card> dealtHand);
-		std::vector<Card>& getHand();
-		void addHand(Card card);
-		int handValue();
+        void clearHand(); // clear hand each round
+        int handValue(); // calculate hand value
+        bool isBust(); // check if you busted
+        bool isBlackjack(); // check for hand score of 21
+        void addHand(Card card);
+
+        // Betting methods
+        bool placeBet(int amount); // places a bet
+        void winBet(); // player wins
+        void loseBet(); // player loses
+
+        // Accessors
+        int getMoney(); // access current amount of money
+        int getCurrentBet(); // accesses current hand
+		std::vector<Card>& getHand(); // Accesses players hand
+
 };
 
 #endif
